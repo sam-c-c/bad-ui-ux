b528c09538e7eaed4b33656da3f783fb1ad2fdc3
Sam Carter <scarter17@hotmail.co.uk>
2023-11-08 12:10:21 +0000

    Added helpful cookie notice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import { Grid, Typography, Button, AppBar, Toolbar, Box } from '@mui/material'
+"use client"
+
+import { Grid, Typography, Button, AppBar, Toolbar, Box, Modal } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../public/logo.png'
+import { useState } from 'react';
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(true);
+  const handleClose = () => setModalOpen(false);
+
   const style = {
     textAlign: 'center', marginBottom: '20px', color: 'white'
+  };
+  const modalStyle = {
+    textAlign: 'center', color: 'white', backgroundColor: 'black', padding: '50px',
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    width: '100%'
   };
 
   return (
@@ -27,6 +40,17 @@ export default function Home() {
           <Link href="/products" style={{color: "#eeeeee", display: 'block', float: 'left'}}>Enter below</Link>
         </Grid>
       </Grid>
+      <Modal
+          open={modalOpen}
+          onClose={handleClose}
+      >
+          <Box sx={modalStyle}>
+             <Typography variant="h6" component="h2">
+                This site doesn&apos;t use cookies.
+          </Typography>
+          <Button variant='contained' onClick={handleClose}>Ok, got it!</Button>
+          </Box>
+       </Modal>
     </>
   )
 }
